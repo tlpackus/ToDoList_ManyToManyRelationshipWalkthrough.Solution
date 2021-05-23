acb1294e09f53dfbbf856e92e4f3003a0214753d
tlpackus <tlpackus@gmail.com>
2021-05-23 15:03:53 -0700

    update to allow splash page and items list page to be viewed in browser

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,26 +1,25 @@
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-//     private readonly ToDoListContext _db;
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     public ItemsController(ToDoListContext db)
-//     {
-//       _db = db;
-//     }
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Item> model = _db.Items.Include(item => item.Category).ToList();
-//       return View(model);
-//     }
+    public ActionResult Index()
+    {
+      return View(_db.Items.ToList());
+    }
 
 //     public ActionResult Create()
 //     {
@@ -36,11 +35,16 @@
 //       return RedirectToAction("Index");
 //     }
 
-//     public ActionResult Details(int id)
-//     {
-//       Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//       return View(thisItem);
-//     }
+    public ActionResult Details(int id)
+    {
+      var thisItem = _db.Items
+        .Include(item => item.JoinEntities)
+        .ThenInclude(join => join.Category)
+        .FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+  }
+}
 
 //     public ActionResult Edit(int id)
 //     {
